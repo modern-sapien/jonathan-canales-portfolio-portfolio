3ebc42d21dc74ad3d2d985e2ed2622c79c32cdbd
Lexx <joncanales2@gmail.com>
2020-11-04 14:39:35 -0500

    Updating portfolio to have a VR skybox background with an unrendered sphere object and then working on creating components.

---
+++ src/containers/VrBackground/VrBackground.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import corona_bk from "./../../images/corona_bk.png"
+import corona_ft from "./../../images/corona_ft.png"
+import corona_lf from "./../../images/corona_lf.png"
+import corona_rt from "./../../images/corona_rt.png"
+import corona_up from "./../../images/corona_up.png"
+import corona_dn from "./../../images/corona_dn.png"
+import * as THREE from "three";
+import OrbitControls from "three-orbitcontrols";
+
+
+class VrBackground extends Component {
+
+    componentDidMount() {
+    }
+    render() {
+        return (
+        <div>Hello this is the background</div>
+        );
+    }
+}
+
+export default VrBackground;
