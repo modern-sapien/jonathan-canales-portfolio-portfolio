769a2a58e42092d73ca774c332edf8db60a823a1
Lexx <joncanales2@gmail.com>
2020-11-04 14:54:08 -0500

    brought in the three js for us as a background with oribt controls.

--- src/containers/VrBackground/VrBackground.jsx
+++ src/containers/VrBackground/VrBackground.jsx
@@ -12,10 +12,72 @@ import OrbitControls from "three-orbitcontrols";
 class VrBackground extends Component {
 
     componentDidMount() {
+          const camera = new THREE.PerspectiveCamera(
+          70,
+          window.innerWidth / window.innerHeight,
+          0.01,
+          1000
+        );
+        camera.position.z = 50;
+    
+        const scene = new THREE.Scene()  
+    
+        let materialArray = []
+        let texture_ft = new THREE.TextureLoader().load(corona_ft)
+        let texture_bk = new THREE.TextureLoader().load(corona_bk)
+        let texture_up = new THREE.TextureLoader().load(corona_up)
+        let texture_dn = new THREE.TextureLoader().load(corona_dn)
+        let texture_rt = new THREE.TextureLoader().load(corona_rt)
+        let texture_lf = new THREE.TextureLoader().load(corona_lf)
+    
+    
+        materialArray.push(new THREE.MeshBasicMaterial({map: texture_ft}))
+        materialArray.push(new THREE.MeshBasicMaterial({map: texture_bk}))
+        materialArray.push(new THREE.MeshBasicMaterial({map: texture_up}))
+        materialArray.push(new THREE.MeshBasicMaterial({map: texture_dn}))
+        materialArray.push(new THREE.MeshBasicMaterial({map: texture_rt}))
+        materialArray.push(new THREE.MeshBasicMaterial({map: texture_lf}))
+    
+        for(let i=0; i < 6; i++)  {
+          materialArray[i].side = THREE.DoubleSide;
+        }
+    
+        let skyboxGeo = new THREE.BoxGeometry(1000, 1000, 1000)
+        let skybox = new THREE.Mesh(skyboxGeo, materialArray)
+        scene.add(skybox);
+    
+        const geometry1 = new THREE.BoxGeometry(4, 4, 4);
+        const material1 = new THREE.MeshNormalMaterial({ wireframe: true });
+        const mesh1 = new THREE.Mesh(geometry1, material1);
+        scene.add(mesh1);
+    
+        const geometry2 = new THREE.SphereGeometry(10, 10, 10);
+        const material2 = new THREE.MeshNormalMaterial({ wireframe: true });
+        const sphere2 = new THREE.Mesh(geometry2, material2);
+        scene.add(sphere2);
+    
+        const renderer = new THREE.WebGLRenderer({ antialias: true });
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(renderer.domElement);
+     
+        const  controls = new OrbitControls(camera, renderer.domElement)
+          controls.minDistance = 1
+          controls.maxDistance = 1000
+    
+        const animate = () => {
+          requestAnimationFrame(animate)
+          sphere2.rotation.x -= .02
+          sphere2.rotation.y -= .02
+    
+          mesh1.rotation.x += .03
+          mesh1.rotation.y += .03
+          renderer.render(scene, camera);
+        }
+        animate()
     }
     render() {
         return (
-        <div>Hello this is the background</div>
+          <div ref={element => this.threeRootElement = element} />
         );
     }
 }
